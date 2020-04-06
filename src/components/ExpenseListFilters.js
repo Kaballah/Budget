c6706d744695e017b3f1f275b8ade9b8db7e3a6c
Kaballah <60391737+Kaballah@users.noreply.github.com>
2020-04-06 12:34:27 +0300

    Added the expense list filter

---
+++ src/components/ExpenseListFilters.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { DateRangePicker } from 'react-dates'
+import { 
+    setTextFilter, 
+    sortByAmount, 
+    sortByDate, 
+    setStartDate, 
+    setEndDate 
+} from '../actions/filters'
+
+
+class ExpenseListFilters extends React.Component {
+    state = {
+        focusedInput: null 
+    }
+
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate))
+    }
+
+    onFocusChange = focusedInput => {
+        this.setState(() => ({ focusedInput }))
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                type="text" 
+                value={this.props.filters.text}
+                onChange={e => {
+                    this.props.dispatch(setTextFilter(e.target.value))
+                }}
+                />
+                <select 
+                    value={this.props.filters.sortBy}
+                    onChange={e => {
+                        if (e.target.value === "date") {
+                            this.props.dispatch(sortByDate())
+                        } else if (e.target.value === "amount") {
+                            this.props.dispatch(sortByAmount)
+                        }
+                    }}
+                    >
+                        <option value="date">Date</option>
+                        <option value="amount">Amount</option>
+                </select>
+                <DateRangePicker 
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.focusedInput}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        filters: state.filters
+    }
+}
+
+export default connect(mapStateToProps)(ExpenseListFilters)
